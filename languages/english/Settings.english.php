ba3f92d9063f8510dc2c551eebd0f3a0c2e1becc
Spuds <spuds@spudsdesign.com>
2024-05-31 11:07:22 -0500

    ! initial commit

---
+++ languages/english/Settings.english.php
@@ -0,0 +1,7 @@
+<?php
+// Version: 1.1; Settings
+
+global $settings;
+
+$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.png';
+$txt['theme_description'] = 'A dark theme with chocolate accents. Gruesome by Antechinus';
